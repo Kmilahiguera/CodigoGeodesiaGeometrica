d46b89d3e26569970da86924063d4a37cfa86019
kmila <camila115011@gmail.com>
2024-09-04 19:24:20 -0500

    v.200

--- codigov1.py
+++ codigov1.py
@@ -1,21 +1,6 @@
-import math as m
+import math 
 
-print ("El problema es de:")
-print( "1. Elipse meridiana")
-print( "2. Elipsoide")
-
-MenuUno= int(input("El problema es de:"))
-if MenuUno==1:          
-    print("Es problema inverso")
-    print("1. SI     2. NO")   
-    MenuInverso= int(input("seleccione"))
-    if MenuInverso==1:
-        
-else:
-     print("Cual es el angulo inical")
-     print("1. w (omega)")
-     print("2. 0 (Teta)")
-     print("1. phi (latitud)")
-     AnguloDado= int(input("seleccione"))
-     if AnguloDado==1:
-          a_=1
+print("DECIR SI ES ELP MERIFIA")
+MenuUno= int(input("Escoja: "))
+if MenuUno==1:
+    print("Cual es el angulo inicial")
